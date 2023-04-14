4003db73716d78f98301fc66c1fc79f5b68a8f6e
Luis Sandoval <95103665+luishmn@users.noreply.github.com>
2023-04-13 23:13:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ from tkinter import *
 from random import uniform
 from tkinter import messagebox
 from tkinter import ttk
+
+class GymCode:
+  def __init__(self):
+    pass
+  
+  
+  
+ GymCode()
+  
+  
+  
